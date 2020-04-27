aed1d1b5c4df5a71ad983bd46d4886998f8b5828
Christopher Venczel <christopher.venczel@outlook.com>
2020-04-26 23:50:05 -0400

    Added favicon and completed default empty sketch.

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="styles.css">
+    <link rel="icon" href="Images/logo.png">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 

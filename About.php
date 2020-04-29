d0e04c2ac81dab1f631b6f341768aa6c262eeef1
Christopher Venczel <christopher.venczel@outlook.com>
2020-04-29 16:01:15 -0400

    Added about page

--- About.php
+++ About.php
@@ -4,12 +4,23 @@
 <head>
     <?php include 'Includes/headInfo.php';?>
 
+    <link rel="stylesheet" href="CSS/about.css">
     <title>About</title>
 </head>
 
 <body>
     <?php include 'Includes/header.php';?>
     <?php include 'Includes/footer.php';?>
+
+    <div id="centerDiv">
+        <p>
+        &emsp;Hi, my name is Christopher Venczel, and I am a second year computer science student at the University of Ottawa.
+        This website is a portfolio of some of the interactive Javascript projects I have created using the <a class="basicLink" target="_blank" href="https://p5js.org/">P5JS</a> library.
+        <br><br>&emsp;The projects feature a variety of different things that interest me including Music 🎵, Design 🎨, Math 📈, Physics 🚀, and AI 🧠.
+        The code for the projects as well as for the website is open-source under the GNU General Public Licence and can be found on my Github 
+        <a target="_blank" class="basicLink" href="https://github.com/chrisvenczel/PortfolioWebsite">here</a>.
+        </p>
+    </div>
 </body>
 
 </html> 

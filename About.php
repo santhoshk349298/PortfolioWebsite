8af0f63902499d8b1bc1f686097fcf7ef572c0f2
Christopher Venczel <christopher.venczel@outlook.com>
2023-06-05 00:00:16 -0400

    Update About.php

--- About.php
+++ About.php
@@ -18,7 +18,7 @@
     <div id="centerDiv">
         <div id="mePic"></div>
         <p id="p1">
-        &emsp;Hi, my name is Christopher Venczel. I am a third year computer science student at the University of Ottawa.
+        &emsp;Hi, my name is Christopher Venczel. I am a recent graduate from the University of Ottawa in Computer Science with an interest in building web apps as well as data visualization and analysis.
         This website is a portfolio of some of the interactive Javascript projects I have created using the <a class="basicLink" target="_blank" href="https://p5js.org/">P5JS</a> library.
         </p>
         
